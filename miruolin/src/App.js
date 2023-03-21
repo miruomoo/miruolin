0435cb8128b9fe7d9b52bc693df327ca71667f1a
Miruo <miruomoo@gmail.com>
2023-03-21 05:05:04 -0400

    Updated Site

--- miruolin/src/App.js
+++ miruolin/src/App.js
@@ -1,11 +1,26 @@
-import './App.css';
+import './App.scss';
+import FadeIn from "react-fade-in/lib/FadeIn";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Miruo's Portfolio</h1>
-      <p>work in progress!</p>
-    </div>
+      <div className="section" id="about">
+      <div className="container">
+        <div className="about-section">
+          <div className="content">
+            <FadeIn bottom>
+              <h1> About Me </h1>
+            </FadeIn>
+            <p>
+              Hey, I'm Miruo Lin, a third-year software engineering student at Western University.
+            </p>
+            <p>sorry!</p>
+            <p>Site currently under construction :)</p>{" "}
+          </div>
+          <div className="image-wrapper">
+          </div>
+        </div>
+      </div>
+      </div>
   );
 }
 
