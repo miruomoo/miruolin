1850e5a6fe3131b53239dcd378948413e679b1d1
Miruo Lin <miruomoo@gmail.com>
2024-04-18 14:54:50 -0400

    Update workData.js

--- miruolin/src/data/workData.js
+++ miruolin/src/data/workData.js
@@ -9,7 +9,7 @@ export default {
       company: "Royal Bank of Canada",
       position: "Software Engineer Intern",
       date: "September 2023 - Current",
-      para: "Developing web applications for the Impact Analysis team using Angular, TypeScript and Java Spring Boot. Used by over 10,000 RBC employees.",
+      para: "Engineered full-stack web applications using Angular, TypeScript and Java Spring Boot. Used by over 1000 RBC mainframe analysts and data engineers.",
       link: "https://www.rbc.com/",
     },
     {
@@ -17,7 +17,7 @@ export default {
       company: "University of Toronto",
       position: "Software Developer Intern",
       date: "May - August 2022",
-      para: "Developed programs for the Engineering Outreach department using Python and C++, generating $25,000 for the faculty.",
+      para: "Developed programs for the Engineering Outreach department using Python and C++, generating $25,000 for the faculty in funding.",
       link: "https://www.utoronto.ca/",
     },
     {
@@ -25,7 +25,7 @@ export default {
       company: "AGI Studio",
       position: "Software Developer Intern",
       date: "May - August 2023",
-      para: "Built internal client tools and a public web application using React and TypeScript, increasing leads and inquiries by 25%.",
+      para: "Built internal client tools and a public web application from wireframes using React, increasing leads and inquiries by 25%.",
       link: "https://www.agistudio.ca/",
     }
   ],
