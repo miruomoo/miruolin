bdea327d3140963773ad7a12541c95d534f00545
Miruo Lin <miruomoo@gmail.com>
2025-11-17 15:45:12 -0500

    Update project cards with tilt effect

--- miruolin/src/components/Project.js
+++ miruolin/src/components/Project.js
@@ -1,17 +1,31 @@
 import React from "react";
 import "../styles/Project.scss";
+import Tilt from "react-parallax-tilt";
 
 const Project = ({ heading, paragraph, imagePic, urlLink, websiteLink=null }) => {
   return (
-    <div
-      className="card"
-      style={{
-        backgroundImage:
-          "linear-gradient(to bottom, rgba(245, 246, 252, 0), rgba(0, 0, 0, 0.5)),url(" +
-          imagePic +
-          ")",
-      }}
+    <Tilt
+      className="tilt-wrapper"
+      tiltMaxAngleX={10}
+      tiltMaxAngleY={10}
+      tiltReverse={true}
+      scale={1.05}
+      transitionSpeed={2000}
+      glareEnable={true}
+      glareMaxOpacity={0.3}
+      glareColor="linear-gradient(135deg, #3794ff, #bea0c7)"
+      glarePosition="all"
+      glareBorderRadius="20px"
     >
+      <div
+        className="card"
+        style={{
+          backgroundImage:
+            "linear-gradient(to bottom, rgba(245, 246, 252, 0), rgba(0, 0, 0, 0.5)),url(" +
+            imagePic +
+            ")",
+        }}
+      >
       <div className="content">
         <h1 className="header">{heading}</h1>
         <p className="text">{paragraph}</p>
@@ -34,6 +48,7 @@ const Project = ({ heading, paragraph, imagePic, urlLink, websiteLink=null }) =>
         </a>}
       </div>
     </div>
+    </Tilt>
   );
 };
 
